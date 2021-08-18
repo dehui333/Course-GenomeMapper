3c093e9e4869c156ed24d3bb390e7ff235f41885
dehui333 <dehui333@gmail.com>
2021-08-18 09:15:20 +0800

    use getopt_long

--- src/mist_mapper.cpp
+++ src/mist_mapper.cpp
@@ -1,5 +1,6 @@
 #include <climits>
 #include <fstream>
+#include <getopt.h>
 #include <iostream>
 #include <string>
 
@@ -8,27 +9,41 @@
 
 
 int main(int argc, char** argv) {
-    
-    
-    if (argc == 2) {
-    //Assume that the user inputs a -h or --version flag    
-        std::string flag = argv[1];
-        if (flag == "-h") {
-            std::cout << "Usage: ./Mist_mapper [path to reference genome data] [path to fragment data 1] [path to fragment data 2] ..." 
-            << std::endl << "Example: ./Mist_mapper GCF_000005845.2_ASM584v2_genomic.fna MAP006-1_2D_pass.fasta MAP006-2_2D_pass.fasta"
-            << std::endl;
-        }
+    int print_version = 0;
+    int print_help = 0;
+    struct option long_options[] = {
+        {"version", no_argument, &print_version, 1},
+        {0, 0, 0, 0}
         
-        if (flag == "--version") {
-            std::cout << "This is version " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << std::endl;
-        }
-    
-    } else {
-    //Assumed that the user inputs sequences data     
-        //std::string reference_path = argv[1];
-        
-        
+    };
+    int option_index = 0;
+    int out;
+    while ((out = getopt_long(argc, argv, ":h", long_options, &option_index)) != -1) {
+        switch (out) {
+            case 0:
+                if (long_options[option_index].flag != 0) {
+                    break;
+                }
+            case 'h':
+                std::cout << "Please enter the path to the reference sequence followed by one or more path(s) to fragment sequences as arguments.\n";
+                std::cout << "e.g. ./mist_mapper ref.fasta frag1.fasta frag2.fasta\n";
+                break;
+            case '?':
+                std::cout << "Unknown option: " << static_cast<char>(optopt) << "\n";
+                break;
+            case ':':
+                std::cout << "Option with no argument: " << static_cast<char>(optopt) << "\n";
+                break;
+            default:
+                break;     
+        }    
     }
+    if (print_version) {
+        std::cout << "The current version is " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << "\n";
+    }
+    
+    
+    
     
     
     return 0;
