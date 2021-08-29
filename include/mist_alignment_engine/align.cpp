58fac4ad92b315c6d68b0c734e2501c0a1eaac87
dehui333 <dehui333@gmail.com>
2021-08-30 02:08:12 +0800

    add skeleton of alignment functions

--- include/mist_alignment_engine/align.cpp
+++ include/mist_alignment_engine/align.cpp
@@ -1,9 +1,77 @@
+#include <iostream>
 
-
+#include "align.h"
 
 namespace mist {
     
+    int Align(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        AlignmentType type,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar,
+        unsigned int* target_begin) {
+        
+        switch (type) {
+            
+            case AlignmentType::global:
+                AlignGlobal(query, query_len, target, target_len, match, mismatch, gap, cigar, target_begin);
+                break;
+            case AlignmentType::local:
+                AlignLocal(query, query_len, target, target_len, match, mismatch, gap, cigar, target_begin);
+                break;
+            case AlignmentType::semi_global:
+                AlignSemiGlobal(query, query_len, target, target_len, match, mismatch, gap, cigar, target_begin);
+                break;
+            default:
+                std::cerr << "Unknown alignment type\n";          
+        }
+        
+    }
     
+    int AlignGlobal(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar,
+        unsigned int* target_begin) {
+            
+        return 0;     
+        
+        
+    }
+    
+    int AlignLocal(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar,
+        unsigned int* target_begin) {
+            
+        return 0;        
+        
+        
+    }
+    
+    int AlignSemiGlobal(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar,
+        unsigned int* target_begin) {
+            
+        return 0;    
+            
+            
+        }
     
     
 }
--- include/mist_alignment_engine/align.h
+++ include/mist_alignment_engine/align.h
@@ -1,13 +1,49 @@
+#include <string>
 
-
+enum class AlignmentType {global, local, semi_global};
 
 
 namespace mist {
+    int Align(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        AlignmentType type,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar = nullptr,
+        unsigned int* target_begin = nullptr
+    );
     
+    int AlignGlobal(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar = nullptr,
+        unsigned int* target_begin = nullptr
+    );
     
+    int AlignLocal(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar = nullptr,
+        unsigned int* target_begin = nullptr
+    );
     
-    
-    
-    
+    int AlignSemiGlobal(  
+        const char* query, unsigned int query_len,
+        const char* target, unsigned int target_len,
+        int match,
+        int mismatch,
+        int gap,
+        std::string* cigar = nullptr,
+        unsigned int* target_begin = nullptr
+    );
+        
     
 }
