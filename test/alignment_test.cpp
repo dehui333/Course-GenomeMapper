3cb47ebca92017bcb235e507ede52de310302b67
dehui333 <dehui333@gmail.com>
2021-08-31 07:55:20 +0800

    try8

--- test/alignment_test.cpp
+++ test/alignment_test.cpp
@@ -7,7 +7,7 @@
 TEST(GlobalAlignmentTest, AllMatch) {
     std::string cigar = "";
     unsigned int target_begin = 1;
-    int score = mist::Align("ATCG", 4, "ATCG", 4, AlignmentType::global, 1, -1, -1, &cigar, &target_begin);
+    int score = mist::Align("ATCG", 4, "ATCG", 4, AlignmentType::global, 1, -1, -1);
     //ASSERT_EQ(score , 4);
     //ASSERT_EQ(cigar, "4=");
     //ASSERT_EQ(target_begin, 0);
