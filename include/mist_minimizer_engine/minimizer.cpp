47563253f3f417d920856bfc15bed9f325d3d5c4
dehui333 <dehui333@gmail.com>
2021-09-04 12:07:45 +0800

    implement find minimizers for multiple sequences and indexing

--- include/mist_minimizer_engine/minimizer.cpp
+++ include/mist_minimizer_engine/minimizer.cpp
@@ -4,7 +4,7 @@
 
 namespace mist {
     //A - 00 C - 01 G - 10 T - 11
-    unsigned int kmer_hash(const char* sequence, unsigned int kmer_len, unsigned int start, bool is_original) {
+    unsigned int KmerHash(const char* sequence, unsigned int kmer_len, unsigned int start, bool is_original) {
         unsigned int hash = 0;
         unsigned int end = start + kmer_len - 1;
         if (!is_original) {
@@ -65,12 +65,15 @@ namespace mist {
         std::set<std::tuple<unsigned int, unsigned int, bool>> minimizers_set;
         
         for (unsigned int window_start = 0; window_start <= sequence_len - window_len; window_start++) {
+            //std::cout << window_start << "\n";
             unsigned int window_min = UINT32_MAX;
             std::set<std::tuple<unsigned int, unsigned int, bool>> minimizers_in_window;
             unsigned int window_end = window_start + window_len - 1;
             for (unsigned int kmer_start = window_start; kmer_start <= window_end - kmer_len + 1; kmer_start++) {
-                unsigned int original_value = kmer_hash(sequence, kmer_len, kmer_start, true);
-                unsigned int reverse_complement_value = kmer_hash(sequence, kmer_len, kmer_start, false);
+                unsigned int original_value = KmerHash(sequence, kmer_len, kmer_start, true);
+                unsigned int reverse_complement_value = KmerHash(sequence, kmer_len, kmer_start, false);
+                //std::cout <<"original " << original_value << "\n";
+                //std::cout <<"complement " << reverse_complement_value << "\n";
                 if (original_value != reverse_complement_value) {
                     unsigned int smaller = original_value < reverse_complement_value ? original_value : reverse_complement_value;
                     bool is_original = original_value < reverse_complement_value ? true : false;
@@ -95,18 +98,38 @@ namespace mist {
     
     void Minimize(
         std::vector<std::string> sequences, 
-        unsigned int sequence_len,
         unsigned int kmer_len,
         unsigned int window_len) {
+        for (unsigned int i = 0; i < sequences.size(); i++) {
+            auto m = Minimize(sequences[i].c_str(), sequences[i].size(), kmer_len, window_len);
+            for (auto t: m) {
+                unsigned int hash_value = std::get<0>(t);
+                if (mist::hash_map.find(hash_value) == mist::hash_map.end()) {
+                    std::vector<std::tuple<unsigned int, unsigned int, bool>> v;
+                    v.push_back(std::make_tuple(i, std::get<1>(t), std::get<2>(t)));
+                    mist::hash_map[hash_value] = v;
+                } else {
+                    mist::hash_map[hash_value].push_back(std::make_tuple(i, std::get<1>(t), std::get<2>(t)));
+                }
+                if (mist::hash_count.find(hash_value) == mist::hash_count.end()) {
+                    mist::hash_count[hash_value] = 1;
+                    total_count++;
+                } else {
+                    mist::hash_count[hash_value]++;
+                    total_count++;
+                }
+                
+            }
             
-            
-            
-    };
-        
-    
-    
-    
-    
-    
-    
+        }       
+    }
 }
+/*
+int main(int argc, char** argv) {
+    std::vector<std::tuple<unsigned int, unsigned int, bool>> m = mist::Minimize("ATCGATCGAT", 10, 3, 5);
+    for (auto p: m) {
+        std::cout << std::get<0>(p) << "\n";
+        std::cout << std::get<1>(p) << "\n";
+        std::cout << std::get<2>(p) << "\n\n";
+    }
+}*/
--- include/mist_minimizer_engine/minimizer.h
+++ include/mist_minimizer_engine/minimizer.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 #include <tuple>
-
+#include <unordered_map>
 
 namespace mist {
     std::vector<std::tuple<unsigned int, unsigned int, bool>> Minimize(
@@ -13,15 +13,17 @@ namespace mist {
         unsigned int kmer_len,
         unsigned int window_len);
         
-    void Minimizer(
+    void Minimize(
         std::vector<std::string> sequences, 
-        unsigned int sequence_len,
         unsigned int kmer_len,
         unsigned int window_len);
     
-    unsigned int kmer_hash(const char* sequence, unsigned int kmer_len, unsigned int start, bool reverse_complement);
+    unsigned int KmerHash(const char* sequence, unsigned int kmer_len, unsigned int start, bool is_original);
+    void Map(const char* sequence, unsigned int sequence_len);
     
-    
+    std::unordered_map<unsigned int, std::vector<std::tuple<unsigned int, unsigned int, bool >>> hash_map;
+    std::unordered_map<unsigned int, unsigned int> hash_count;
+    unsigned int total_count = 0;
     
     
     
